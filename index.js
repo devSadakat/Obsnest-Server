28d67146b394305caf48b17ba8fdbd328eee908a
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-04-10 15:07:22 +0700

    Get Data form mongoDb

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect With MongoDB //
-// // // // // // // // //---------------------// // // // // // // // // //
+//___________________________________ __// MongoDB //________________________________//
+// /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ /---/ //
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@s-cluster0.pvehgyn.mongodb.net/?retryWrites=true&w=majority&appName=S-Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -24,18 +24,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const collectedData = client.db('obsnest').collection('procuctData')
+
+        //_________________________________// Get Operation //_____________________________//
+        app.get('/data', async (req, res) => {
+            const result = collectedData.find().toArray;
+            res.send(result);
+        })
+
     } finally {
-        // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-// // // // // // // // //---------------------// // // // // // // // // //
+// // // // // // // // //----------END MongoDb-----------// // // // // // // // // //
 
 
 // Get Api
