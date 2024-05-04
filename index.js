11bd04918b67ca44b5de23800d8edd7255ee6aa3
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-05-04 20:49:18 +0700

    Added Mongodb Connection code

--- index.js
+++ index.js
@@ -28,6 +28,40 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        // MongoDb Connection
+
+
+        const { MongoClient, ServerApiVersion } = require('mongodb');
+        const uri = "mongodb+srv://SadakatObsnest:<password>@atlascluster.jhrstoy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+
+        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+
+        async function run() {
+            try {
+                // Connect the client to the server	(optional starting in v4.7)
+                await client.connect();
+                // Send a ping to confirm a successful connection
+                await client.db("admin").command({ ping: 1 });
+                console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            } finally {
+                // Ensures that the client will close when you finish/error
+                await client.close();
+            }
+        }
+        run().catch(console.dir);
+
+
+        // MongoDb Connecteed
+
+
+
         const productCollection = client.db('obsnest').collection('productData');
 
         app.get('/', async (req, res) => {
