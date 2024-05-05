d8a72bb8cfb1dee4f5ca6f1e0e9a22cdba8d9461
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-05-05 21:21:26 +0700

    Conneced With MongoDB Succesfuly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require ('dotenv').config();
+require('dotenv').config();
 const app = express();
 
 // Port
@@ -10,13 +10,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// Start Mongodb Connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.jhrstoy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Connected with MongoDB
+
+
 // Get Apies
-app.get('/', async(req, res)=>{
-    const result =await "Obsnest Banckend Server Is Running Propperly";
+app.get('/', async (req, res) => {
+    const result = await "Obsnest Banckend Server Is Running Propperly";
     res.send(result);
 });
 
 // Listen Apies
 app.listen(port, () => {
-        console.log(`Started Server on port${port}`);
+    console.log(`Started Server on port${port}`);
 });
