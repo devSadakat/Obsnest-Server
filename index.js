19aa2d5b1598f845ab427c8bdc105322acebf511
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-05-13 19:52:35 +0700

    Resolved Vercel Problem

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: '*'
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
